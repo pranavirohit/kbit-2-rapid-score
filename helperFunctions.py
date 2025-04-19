305c74a73dc4be366e3ce9effb9fccab0a5e0fda
prohit-cmu <prohit@andrew.cmu.edu>
2025-04-19 09:26:56 -0400

    Wrote first tesseract helper function in helperFunctions.py
    Also updated library naming for pytesseract

--- helperFunctions.py
+++ helperFunctions.py
@@ -1,10 +1,14 @@
 import cv2
+import pytesseract as pyt
 
 def processImage(filePath):
     image = cv2.imread(filePath)
     grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     threshold = (grayImage, 150, 255, cv2.THRESH_BINARY_INV)
-    finalImage = threshold[1]
+    processedImage = threshold[1]
 
-    return finalImage
+    return processedImage
 
+def extractAllText(image):
+    text = pyt.image_to_string(image, config = '--psm 6')
+    return text
---
+++ src/main.py
@@ -0,0 +1,7 @@
+# from [fileName] import [functionName]
+import cv2 # Importing OpenCV
+import pytesseract as pyt # Importing Tesseract
+from cmu_graphics import* # Importing CMU graphics
+
+from helperFunctions import*
+
