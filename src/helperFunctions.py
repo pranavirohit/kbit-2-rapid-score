4d695bb293229d2af3792e2ca7da7a6404604b81
prohit-cmu <prohit@andrew.cmu.edu>
2025-04-19 13:21:27 -0400

    Updating method to extract data (rewriting splitImage function as detectVerticalLines)
    Instead of manually splitting the page, I want to write a program that detects where the vertical lines are on the page. For every page, the same vertical lines correspond to the same "cut-off" points, so I can use this to make the later text extraction process a lot easier.

--- src/helperFunctions.py
+++ src/helperFunctions.py
@@ -23,31 +23,6 @@ This file has the main helper functions I'm using to automate KBIT-2 scoring.
 """
 from commonImports import *
 
-def splitImage(filePath):
-    # Read in image and turn into grayscale version
-    image = cv2.imread(filePath)
-    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Locate where to split image so I can process the Nonverbal Table and Verbal Table seperately
-    height, width = grayImage.shape
-    splitCol = int(width * (2/3))
-
-    # gray[rows, cols]
-    verbalArray = grayImage[:, 0:splitCol] # Process all rows, but stop at the midpoint column
-    nonVerbalArray = grayImage[:, splitCol:] # Process all rows, but START at the midpoint column
-
-    return verbalArray, nonVerbalArray
-
-def processImage(array):
-    _, binaryImage = cv2.threshold(array, 150, 255, cv2.THRESH_BINARY_INV)
-    processedImage = PILImage.fromarray(binaryImage)
-
-    return processedImage
-
-def extractAllText(image):
-    text = pyt.image_to_string(image, config = '--psm 6')
-    return text
-
 def processPDF(pdfPath, outputFolder):
     # Convert each PDF page to an image
     images = convert_from_path(pdfPath, dpi = 300)
@@ -63,6 +38,38 @@ def processPDF(pdfPath, outputFolder):
         image.save(fullPath, 'PNG') # Use Pillow for this
 
         print(f'Added {fileName} to {fullPath}')
+
+
+def splitImage(filePath):
+    # Read in image and turn into grayscale version
+    image = cv2.imread(filePath)
+    grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # Locate where to split image so I can process the Nonverbal Table and Verbal Table seperately
+    height, width = grayImage.shape
+    splitCol = int(width * (2/3))
+
+    # gray[rows, cols]
+    verbalArray = grayImage[:, 0:splitCol] # Process all rows, but stop at the midpoint column
+    nonVerbalArray = grayImage[:, splitCol:] # Process all rows, but START at the midpoint column
+
+    return verbalArray, nonVerbalArray
+
+def detectVerticalLines():
+  # Read in image and turn into grayscale version
+  image = cv2.imread(filePath)
+  grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+  # Continue here
+
+def processImage(array):
+    _, binaryImage = cv2.threshold(array, 150, 255, cv2.THRESH_BINARY_INV)
+    processedImage = PILImage.fromarray(binaryImage)
+
+    return processedImage
+
+def extractAllText(image):
+    text = pyt.image_to_string(image, config = '--psm 6')
+    return text
 
 # Want to eventually name the Table B1 files by their age
 def getTableAge(image):
