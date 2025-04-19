ebc0bcd89247b52af62b75c503322c67aafcce05
s-prohit <144193386+s-prohit@users.noreply.github.com>
2025-04-18 22:30:50 -0400

    Added information on committing helper functions to main.py, completed GitHub Training Part 1

--- src/main.py
+++ src/main.py
@@ -1,2 +1,4 @@
+# from [fileName] import [functionName]
+
 def foo():
     return 42
---
+++ src/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, score):
+        self.score = score
+    
+    def resetScore(self, score):
+        self.score = 0
