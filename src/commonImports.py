e34c86ca71444dc8cf66e19d826e98aec86b9b7c
prohit-cmu <prohit@andrew.cmu.edu>
2025-04-19 16:44:33 -0400

    Debugging issue across accessing functions (why my code can sometimes access the helper functions and when it can't)

--- src/commonImports.py
+++ src/commonImports.py
@@ -6,5 +6,6 @@ from PIL import Image as PILImage
 from cmu_graphics import* # CMU graphics
 from pdf2image import convert_from_path #PDF2Image
 
-from helperFunctions import *
-from debuggingFunctions import *
+from helperFunctions import (processPDF, splitImage, processImage,
+extractAllText, getTableAge, getVerticalLinesPositions, thresholdImage,
+isolateVerticalLines, splitThreeTables, findMidpoint)                     
--- src/imageData.py
+++ src/imageData.py
@@ -1,7 +1,10 @@
 from commonImports import *
 
+
+
+
 testPage = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\data_files\page_78.png"
-verbalTest, nonVerbalTest = splitImage(testPage)
+# verbalTest, nonVerbalTest = splitImage(testPage)
 
 # Prints out a column of all the vertical lines!!
 print(getVerticalLinesPositions(testPage))
