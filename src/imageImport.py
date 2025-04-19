473098b598befd553b96198951d2c086764f277d
prohit-cmu <prohit@andrew.cmu.edu>
2025-04-19 16:33:44 -0400

    Updated imageImport.py

--- src/imageImport.py
+++ src/imageImport.py
@@ -10,9 +10,7 @@ Not intended to be part of the core pipeline — just useful for preparing data.
 from commonImports import *
 
 def saveAllKBITPages():
-    # Test page
     pdfKBIT = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\starter_files\KBIT_Pages_78_84.pdf"
-    # Save images here
     allKBITimages = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\data_files"
     processPDF(pdfKBIT, allKBITimages)
 
