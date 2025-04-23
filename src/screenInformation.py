a064cad1dbda387b69350c2b2591bf8723610568
prohit-cmu <prohit@andrew.cmu.edu>
2025-04-23 00:10:30 -0400

    Added switching screen functionality

--- src/screenInformation.py
+++ src/screenInformation.py
@@ -1,10 +1,10 @@
 from commonImports import *
 
-screenNames = ['start', 'info', 'template', 'upload', 'output1',
-               'output2', 'output3', 'results', 'end' ]
+
 
 def onAppStart(app):
-    app.filePath = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\user_experience\KBIT_2_Start_Screen.png"
+    app.screenNames = ['start', 'info', 'template', 'upload', 'output1',
+               'output2', 'output3', 'results', 'end' ]
     app.width, app.height = 960, 540
     app.cx, app.cy = app.width // 2, app.height // 2
     app.buttonCenters = []
@@ -12,13 +12,24 @@ def onAppStart(app):
 def start_redrawAll(app):
     image = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\user_experience\KBIT_2_Start_Screen.png"
     drawImage(image, app.cx, app.cy, align = 'center')
-    rectCy, rectWidth, rectHeight = 400, 300, 40
-    # drawRect(app.cx, rectCy, rectWidth, rectHeight, align = 'center', fill = 'cyan')
-    # drawLabel('press space to begin', app.cx, rectCy, size = 30)
-
-# def onMousePress(app, mouseX, mouseY):
     
-# def onResiz
+def start_onKeyPress(app, key):
+    if key == 'space':
+        setActiveScreen(app.screenNames[1])
+
+def info_redrawAll(app):
+    image = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\user_experience\KBIT_2_Screen_2_Info.png"
+    drawImage(image, app.cx, app.cy, align = 'center')
+
+def info_onKeyPress(app, key):
+    if key == 'space':
+        setActiveScreen(app.screenNames[2])
+    if key == 'backspace':
+        setActiveScreen(app.screenNames[0])
+
+def template_redrawAll(app):
+    image = r"C:\Users\pkroh\OneDrive - andrew.cmu.edu\2024-25\15-112\Term Project\user_experience\KBIT_2_Screen_3_Template.png"
+    drawImage(image, app.cx, app.cy, align = 'center')
 
 def main():
     runAppWithScreens(initialScreen='start')
